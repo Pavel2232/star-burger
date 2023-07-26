ITerekhov98/burgershop

refactor restorator UI

foodcartapp/views/AuthViews.py
@@ -1,71 +1,46 @@
-from django.contrib.auth import authenticate
-from django.contrib.auth import login
-from django.contrib.auth import logout
-from django.contrib.auth.models import Group
-from django.contrib.auth.models import User
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import redirect
 from django.shortcuts import render
 from django.views import View
+from django.urls import reverse_lazy
+
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
+from django.contrib.auth import views as auth_views
 
 from foodcartapp.forms.AuthForms import Login
 from foodcartapp.forms.AuthForms import Signup
 
 
-class SignUpView(View):
-    def get(self, request, *args, **kwargs):
-        form = Signup()
-        return render(request, "sign_up.html", context={'title': 'SignUp', 'form': form})
-
-    def post(self, request):
-        form = Signup(request.POST)
-
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-
-            user = User.objects.create_user(username=username, password=password)
-            user.customer_profile.phone_number = form.cleaned_data['phone_number']
-            user.customer_profile.address = form.cleaned_data['address']
-
-            staff = Group.objects.get(name="RestaurantAdmins")
-            staff.user_set.add(user)
-            user.is_staff = True
-            user.save()
-            user = authenticate(request, username=username, password=password)
-
-            if user is not None:
-                login(request, user)
-                return redirect('foodcartapp:RestaurantView')
-            else:
-                raise PermissionDenied
-
-
 class LoginView(View):
     def get(self, request, *args, **kwargs):
-        if(request.user.is_authenticated):
-            if request.user.is_staff:
-                return redirect("foodcartapp:RestaurantView")
-            raise PermissionDenied
-
         form = Login()
-        return render(request, "login.html", context={'title': 'Login | User', 'form': form})
+        return render(request, "login.html", context={
+            'title': 'Login | User',
+            'form': form
+        })
 
     def post(self, request):
         form = Login(request.POST)
+
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
 
             user = authenticate(request, username=username, password=password)
-            if user is not None:
+            if user:
                 login(request, user)
                 if user.is_staff:
                     return redirect("foodcartapp:RestaurantView")
-            raise PermissionDenied
+                return redirect("start_page")
+
+        return render(request, "login.html", context={
+            'title': 'Login | User',
+            'form': form
+        })
 
 
-class LogoutView(View):
-    def get(self, request):
-        logout(request)
-        return redirect("foodcartapp:Login")
+class LogoutView(auth_views.LogoutView):
+    next_page = reverse_lazy('foodcartapp:login')

Pavel2232/star-burger

foodcartapp/views/AuthViews.py
@@ -1,10 +1,13 @@
 from django.contrib.auth import authenticate
 from django.contrib.auth import login
 from django.contrib.auth import logout
-from django.views import View
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
 from django.core.exceptions import PermissionDenied
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User, Group
+from django.shortcuts import redirect
+from django.shortcuts import render
+from django.views import View
+
 from foodcartapp.forms.AuthForms import Login
 from foodcartapp.forms.AuthForms import Signup
 