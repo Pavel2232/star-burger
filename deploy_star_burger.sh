25e9316f5dd5828a26dc64af28754d4274d0b3f1
Pavel2232 <106510078+Pavel2232@users.noreply.github.com>
2023-09-17 11:23:07 +0300

    refacto deploy script

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -13,4 +13,20 @@ trap 'npm ci --dev' ERR
 trap './node_modules/.bin/parcel bundles-src/index.js --dist-dir bundles --public-url="./"' ERR
 echo 'Node.js run'
 trap 'systemctl daemon-reload' ERR
+HASH=$(git rev-parse HEAD)
+tarp 'curl --request POST \
+     --url https://api.rollbar.com/api/1/deploy \
+     --header 'X-Rollbar-Access-Token: $HASH' \
+     --header 'accept: application/json' \
+     --header 'content-type: application/json' \
+     --data '
+{
+  "environment": "production",
+  "revision": "67a966bf87f1e6d35728e92c0580b88feb4234e3",
+  "rollbar_username": "string",
+  "local_username": "string",
+  "comment": "deploy",
+  "status": "string"
+}
+''
 exec "$@"
